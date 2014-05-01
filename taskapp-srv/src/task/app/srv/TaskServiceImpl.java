9c2cca70db3da8f7c5054827587d04859c179192
Wolfgang Imig <jsfsproject@gmail.com>
2014-05-01 16:57:59 +0200

    Notify implementation

--- taskapp-srv/src/task/app/srv/TaskServiceImpl.java
+++ taskapp-srv/src/task/app/srv/TaskServiceImpl.java
@@ -37,8 +37,8 @@ public class TaskServiceImpl extends BSkeleton_TaskService {
 		}
 		
 		// Notify client 
-//		BClient_Taskapp bclient = (BClient_Taskapp) session.getClientR();
-//		bclient.getTaskNotify().receiveTask(cloneTask(task));
+		BClient_Taskapp bclient = (BClient_Taskapp) session.getClientR();
+		bclient.getTaskNotify().receiveTask(cloneTask(task));
 		
 	}
 
